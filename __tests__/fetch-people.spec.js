873b2541102eef6c92b594ffd1d73cab739f84d1
Craig Rodrigues <craig.rodrigues@turner.com>
2021-07-24 09:31:11 -0400

    Make paging test

--- __tests__/fetch-people.spec.js
+++ __tests__/fetch-people.spec.js
@@ -2,15 +2,61 @@
 
 const axios = require('axios');
 const { fetchPeople } = require('../app');
+const samplePeopleData = require('./data/sample-people-data.json');
 
 jest.mock('axios');
 
+function makePagingResponses(num) {
+    const response = {
+        data: {
+            metadata: {
+                paging: {
+                    current_page: 1,
+                    next_page: 2
+                }
+            },
+            data: []
+        },
+        headers: {
+            etag: '',
+            date: ''
+        }
+    };
+
+    const responses = new Array(num).fill('').map((r) => JSON.parse(JSON.stringify(response)));
+
+    responses.forEach((r, i) => {
+        r.data.metadata.paging.current_page = i + 1;
+        r.data.metadata.paging.next_page = i + 2 > num ? null : i + 2;
+    });
+
+    return responses;
+}
+
 describe('fetchPeople', () => {
-    it('should fetch people', async () => {
+    it ('should page correctly', async () => {
+        const responses = makePagingResponses(5);
+
+        axios.get.mockResolvedValueOnce(responses[0]);
+        axios.get.mockResolvedValueOnce(responses[1]);
+        axios.get.mockResolvedValueOnce(responses[2]);
+        axios.get.mockResolvedValueOnce(responses[3]);
+        axios.get.mockResolvedValueOnce(responses[4]);
+
+        await fetchPeople();
+        expect(axios.get).toHaveBeenCalledTimes(6);
+    });
+
+    it('should fetch people and map properties correctly', async () => {
         const response = {
             data: {
-                metadata: {},
-                data: []
+                metadata: {
+                    paging: {
+                        current_page: 1,
+                        next_page: null
+                    }
+                },
+                data: samplePeopleData
             },
             headers: {
                 etag: '',
@@ -18,6 +64,16 @@ describe('fetchPeople', () => {
             }
         };
 
-        axios.get.mockResolvedValue(response)
+        axios.get.mockResolvedValue(response);
+
+        const people = await fetchPeople();
+
+        people.forEach((person) => {
+            expect(person).toEqual(expect.objectContaining({
+                name: expect.any(String),
+                email: expect.any(String),
+                title: expect.any(String)
+            }));
+        });
     });
 })
