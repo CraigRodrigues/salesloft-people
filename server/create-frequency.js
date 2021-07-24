d76c9f33c5fbdfeb084de7f3159ce4140f1b2f3e
Craig Rodrigues <craig.rodrigues@turner.com>
2021-07-24 16:16:26 -0400

    Add orderBy and use for frequencyArray

--- server/create-frequency.js
+++ server/create-frequency.js
@@ -1,20 +1,27 @@
 'use strict';
 
+const orderBy = require('lodash.orderby');
+
 // assumption: we want a map of every character in an email including @ and .
 // frequency count of all the unique characters in all the email addresses of all the People you have access to
 // sorted by frequency count
 function createFrequencyMap(people) {
-    const map = {};
+    const charMap = {};
     const emails = people.map((p) => p.email);
 
     emails.forEach((email) => {
         for (const char of email) {
-            map[char] ? map[char] += 1 : map[char] = 1;
+            charMap[char] ? charMap[char] += 1 : charMap[char] = 1;
         }
     });
 
-    // TODO: sort by key then by value
-    return map;
+    const frequencyArray = [];
+
+    for (const [character, count] of Object.entries(charMap)) {
+        frequencyArray.push({ character, count });
+    }
+
+    return orderBy(frequencyArray, ['count', 'character'], ['desc']);
 }
 
 module.exports = { createFrequencyMap };
