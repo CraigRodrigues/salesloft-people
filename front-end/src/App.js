fdc62d82750ced8361a8ba676026a09a041f01d7
Craig Rodrigues <craig.rodrigues@turner.com>
2021-07-24 14:25:13 -0400

    Add simple hooks for data type selected

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
+  theme,
   ChakraProvider,
   Box,
   Grid,
   ButtonGroup,
   Button,
-  theme,
+  Table
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 function App() {
+  const [dataType, setDataType] = useState('all');
+
+  useEffect(() => {
+    console.log(dataType);
+  }, [dataType]);
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <ColorModeSwitcher justifySelf="flex-end" />
         <Grid minH="100vh" templateRow="repeat(2, 1fr)">
           <ButtonGroup varient="outline" spacing="6" align="center">
-            <Button colorScheme="blue" variant="outline">
+            <Button colorScheme="blue" variant="outline" onClick={() => setDataType('all')}>
               All People
             </Button>
-            <Button colorScheme="blue" variant="outline">
+            <Button colorScheme="blue" variant="outline" onClick={() => setDataType('frequency')}>
               Character Frequency
             </Button>
-            <Button colorScheme="blue" variant="outline">
+            <Button colorScheme="blue" variant="outline" onClick={() => setDataType('duplicates')}>
               Possible Duplicates
             </Button>
           </ButtonGroup>
+          <Table></Table>
         </Grid>
       </Box>
     </ChakraProvider>
