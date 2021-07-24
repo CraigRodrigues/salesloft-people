edcc3a57bb09d0787d80f93786ddcd9fda67435f
Craig Rodrigues <craig.rodrigues@turner.com>
2021-07-24 13:31:11 -0400

    Fix npm script and writeFile

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const fs = require('fs');
+const { exec } = require('child_process');
+const CronJob = require('cron').CronJob;
 const { fetchPeople } = require('./fetch-people');
 const { createFrequencyMap } = require('./create-frequency');
 const { findDuplicates } = require('./find-duplicates');
@@ -8,29 +10,29 @@ const { findDuplicates } = require('./find-duplicates');
 async function run() {
     // Fetch initial data from SalesLoft API
     const people = await fetchPeople();
-    console.log('People fetched: %j', people);
 
     // Create People.json file in cache
-    fs.writeFileSync('./cache/people.json', JSON.stringify(people, 0, 2));
+    fs.writeFileSync(`${__dirname}/cache/people.json`, JSON.stringify(people, 0, 2));
     console.log('people.json cached');
 
     // Use people.json data to create a frequency map
     const frequencyMap = createFrequencyMap(people);
 
-    console.log('Frequency map created: %j', frequencyMap);
-    fs.writeFileSync('./cache/frequency.json', JSON.stringify(frequencyMap, 0, 2));
+    fs.writeFileSync(`${__dirname}/cache/frequency.json`, JSON.stringify(frequencyMap, 0, 2));
     console.log('freqency.json cached');
 
     // Use people.json data to create a possible duplicates list
     const duplicates = findDuplicates(people);
 
-    console.log('Potential duplicates found: %j', duplicates);
-    fs.writeFileSync('./cache/duplicates.json', JSON.stringify(duplicates, 0, 2));
+    fs.writeFileSync(`${__dirname}/cache/duplicates.json`, JSON.stringify(duplicates, 0, 2));
     console.log('duplicates.json cached');
 
     // Begin cron job
-
-    // Start api server
+    // new CronJob('', async () => {
+    //     const { stdout, stderr } = await exec('node api.js');
+    //     console.log('stdout:', stdout);
+    //     console.error('stderr:', stderr);
+    // });
 }
 
 run().catch(e => {
