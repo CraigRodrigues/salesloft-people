cdf31bb59eaf5eb07b011bc3e75d46296137cce4
Craig Rodrigues <craig.rodrigues@turner.com>
2021-07-24 09:42:07 -0400

    Fix fetchPeople to map data correctly

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 
 const axios = require('axios');
 axios.defaults.baseURL = 'https://api.salesloft.com/v2/people.json';
-axios.deafults.headers.common['Authorization'] = `Bearer ${process.env.SALES_LOFT_API_KEY}`;
+axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.SALES_LOFT_API_KEY}`;
 
 // let etag = '';
 // let if_modified_since = '';
 
 async function fetchPeople() {
-    const data = [];
+    let results = [];
     let next_page = 1;
 
     // while we still have pages continue to gather data and map into an array of people
@@ -20,24 +20,39 @@ async function fetchPeople() {
         const response = await axios.get('https://api.salesloft.com/v2/people.json', config);
 
         next_page = response.data.metadata.paging.next_page;
-        console.log(next_page);
-        console.log(response.data.data);
+
+        const mappedResults = response.data.data.map((person) => {
+            return {
+                name: person.display_name,
+                email: person.email_address,
+                title: person.title,
+                herp: 'derp'
+            }
+        });
+
+        results = [...results, ...mappedResults];
 
         // perhaps store the ETag and date for future use
         // etag = response.headers.etag;
         // if_modified_since = response.headers.date;
     }
 
+    console.log(results);
+    return results;
 }
 
 async function run() {
-    const people = await fetchPeople();
-    console.log(people)
     // Fetch initial data from SalesLoft API
+    const people = await fetchPeople();
+
     // Create People.json file in cache
+
     // Use people.json data to create a frequency map
+
     // Use people.json data to create a possible duplicates list
+
     // Begin cron job
+
     // Start api server
 }
 
