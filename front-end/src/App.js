0dee43af4867e6667cad901e6bc15f8f82423de3
Craig Rodrigues <craig.rodrigues@turner.com>
2021-07-24 14:20:11 -0400

    Add buttons

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,37 +2,30 @@ import React from 'react';
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
   Grid,
+  ButtonGroup,
+  Button,
   theme,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <Grid minH="100vh" templateRow="repeat(2, 1fr)">
+          <ButtonGroup varient="outline" spacing="6" align="center">
+            <Button colorScheme="blue" variant="outline">
+              All People
+            </Button>
+            <Button colorScheme="blue" variant="outline">
+              Character Frequency
+            </Button>
+            <Button colorScheme="blue" variant="outline">
+              Possible Duplicates
+            </Button>
+          </ButtonGroup>
         </Grid>
       </Box>
     </ChakraProvider>
