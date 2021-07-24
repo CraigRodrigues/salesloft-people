b88c92c0e65a5d743a1cdf6f003e07e7fe0b096e
Craig Rodrigues <craig.rodrigues@turner.com>
2021-07-24 07:57:56 -0400

    Stub out app and api with comments

---
+++ api.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// Create express app with three routes
+// GET for people
+// GET for frequency
+// GET for duplicates
---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Fetch initial data from SalesLoft API
+// Create People.json file in cache
+// Use people.json data to create a frequency map
+// Use people.json data to create a possible duplicates list
+// Begin cron job
+// Start api server
