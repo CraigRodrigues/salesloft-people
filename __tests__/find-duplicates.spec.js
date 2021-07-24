89ab77dd7bb7056c28ebec37d9de30a776f10f7e
Craig Rodrigues <craig.rodrigues@turner.com>
2021-07-24 12:10:35 -0400

    Stub tests for findDuplicates

--- __tests__/find-duplicates.spec.js
+++ __tests__/find-duplicates.spec.js
@@ -72,3 +72,101 @@ describe('isDuplicate', () => {
         expect(actual).toBe(false);
     });
 });
+
+describe('findDuplicates', () => {
+    const duplicatePeople = [
+        {
+            name: 'Steven Pease',
+            email: 'sakatius@gmail.com',
+            title: 'Software Engineer'
+        },
+        {
+            name: 'Possibly Duplicate',
+            email: 'sakatiuss@gmail.com',
+            title: 'My Job'
+        },
+        {
+            name: 'SomethingNewHere Here1',
+            email: 'last@me.com',
+            title: 'Baby Yoda Keeper'
+        },
+        {
+            name: 'Something New Newer',
+            email: 'newemailisnew@menew.com',
+            title: 'My Job Is New'
+        },
+        { name: 'Testers Two', email: 'testt@gmail.com', title: 'Testing' },
+        {
+            name: 'Keagan Tromp',
+            email: 'raa_beetty@google.com',
+            title: 'Central Assurance Administrator'
+        },
+        {
+            name: 'Sheridan Bogisich',
+            email: 'erik@lubowitz.name',
+            title: 'Lead Applications Planner'
+        },
+        {
+            name: 'Marisa Casper',
+            email: 'amixe@lindgren.info',
+            title: 'Direct Security Representative'
+        },
+        {
+            name: 'Griffin Hand',
+            email: 'mamixe@lindgren.info',
+            title: 'International Usability Agent'
+        },
+        {
+            name: 'Mikel Reynolds',
+            email: 'george_aiegwnd@boyer.name',
+            title: 'Global Solutions Technician'
+        }
+    ];
+
+    it('should find duplicates', () => {
+        const actual = findDuplicates(duplicatePeople);
+        const expected = [
+            {
+                name: 'Steven Pease',
+                email: 'sakatius@gmail.com',
+                title: 'Software Engineer'
+            },
+            {
+                name: 'Possibly Duplicate',
+                email: 'sakatiuss@gmail.com',
+                title: 'My Job'
+            },
+            {
+                name: 'Marisa Casper',
+                email: 'amixe@lindgren.info',
+                title: 'Direct Security Representative'
+            },
+            {
+                name: 'Griffin Hand',
+                email: 'mamixe@lindgren.info',
+                title: 'International Usability Agent'
+            }
+        ];
+
+        expect(actual).toEqual(expected);
+    });
+
+    it('should not find any duplicates', () => {
+        const actual = findDuplicates(uniquePeople);
+        const expected = [];
+
+        expect(actual).toEqual(expected);
+    });
+
+    it('should not find any duplicates if people is empty', () => {
+        const actual = findDuplicates([]);
+
+        expect(actual).toEqual([]);
+    });
+
+    it('should not find any duplicates if people is null', () => {
+        const actual = findDuplicates(null);
+
+        expect(actual).toEqual([]);
+    });
+});
