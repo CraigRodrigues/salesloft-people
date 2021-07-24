90368f60274ffbfca6931b68a67fe12c5a9dd672
Craig Rodrigues <craig.rodrigues@turner.com>
2021-07-24 12:57:32 -0400

    Stand up basic express server with logging

--- back-end/api.js
+++ back-end/api.js
@@ -1,6 +1,38 @@
 'use strict';
 
+const PORT = process.env.PORT || '3000';
+const HOST = process.env.HOST || 'localhost.com';
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('combined'));
+
 // Create express app with three routes
+app.get('/', (req, res) => {
+    res.send('hello');
+});
+
 // GET for people
+app.get('/people.json', (req, res) => {
+    res.send('people');
+});
+
 // GET for frequency
+app.get('/frequency.json', (req, res) => {
+    res.send('frequency');
+});
+
 // GET for duplicates
+app.get('/duplicates.json', (req, res) => {
+    res.send('duplicates');
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening at ${HOST}:${PORT}`);
+});
+
+module.exports = app;
