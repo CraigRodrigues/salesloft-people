6636ddd81c790265bcc434f0ac31a19a61740a98
Craig Rodrigues <craig.rodrigues@turner.com>
2021-07-26 01:46:33 -0400

    Finish adding components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import logo from './landing_logo.svg';
 import {
   theme,
   ChakraProvider,
-  Box,
-  Heading,
-  Tabs, TabList, TabPanels, Tab, TabPanel
+  Image,
+  Flex,
+  Spacer,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Text,
+  Link,
 } from '@chakra-ui/react';
 
 import { ColorModeSwitcher } from './ColorModeSwitcher';
@@ -15,29 +23,37 @@ import Duplicates from './Duplicates';
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box>
-        <ColorModeSwitcher />
-        <Heading as="h1" size="2xl">SalesLoft People</Heading>
-      </Box>
-      <Tabs isLazy>
-        <TabList>
-          <Tab>All People</Tab>
-          <Tab>Character Frequency</Tab>
-          <Tab>Possible Duplicates</Tab>
-        </TabList>
+      <Flex margin="20px" flexDirection="column">
+        <Flex margin="20px">
+          <Image src={logo} />
+          <Spacer />
+          <ColorModeSwitcher />
+        </Flex>
+        <Tabs isLazy>
+          <TabList>
+            <Tab>All People</Tab>
+            <Tab>Character Frequency</Tab>
+            <Tab>Possible Duplicates</Tab>
+          </TabList>
 
-        <TabPanels>
-          <TabPanel>
-            <PeopleTable />
-          </TabPanel>
-          <TabPanel>
-            <FrequencyTable />
-          </TabPanel>
-          <TabPanel>
-            <Duplicates />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
+          <TabPanels>
+            <TabPanel>
+              <PeopleTable />
+            </TabPanel>
+            <TabPanel>
+              <FrequencyTable />
+            </TabPanel>
+            <TabPanel>
+              <Duplicates />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Flex>
+      <Text marginBottom="10px" textAlign="center">
+        Made for SalesLoft by {" "}
+          <Link color="blue.500" href="https://www.linkedin.com/in/craigrodrigues" isExternal>Craig Rodrigues</Link>
+        {" "} 🤘🏾
+      </Text>
     </ChakraProvider>
   );
 }
